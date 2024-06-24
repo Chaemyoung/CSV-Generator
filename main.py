2f9d65c89a2cdb07f880aaca762a47b64408d207
Chaemyoung <chaemyoung2015@gmail.com>
2024-06-23 21:50:29 -0700

    Update by add ANSI codes for colors

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import logging
 from dotenv import load_dotenv
 from openai import OpenAI
 
+# ANSI escape codes for colors
+RESET = "\033[0m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+
+# Text Colors
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
 load_dotenv()
 
 # Set up logging
